fa7f538add80a10c408042f2717d58fd1626231a
dissfall <dissfalling@gmail.com>
2019-08-03 14:06:03 +0300

    Prettyfy source
    See: https://github.com/Dissfall/winston-microlog/projects/1#card-24756145

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,32 @@
 import { Logger, createLogger, transports, format } from 'winston';
 import appRoot from 'app-root-path';
 
-
 const { combine, timestamp, label, printf } = format;
 
 const tagLen: number = 8;
-const faceLen: number = 6
+const faceLen: number = 6;
 
-const levelSign: {[propName: string]: string} = {
+const levelSign: { [propName: string]: string } = {
   info: 'ℹ️ ',
   warn: '⚠️ ',
   error: '⛔️',
   debug: '☢️ '
-}
-
+};
 
 class ServiceLogger {
   private serviceName: string = '';
-  private instanceName: string = ''; 
+  private instanceName: string = '';
   private path: string = 'logs';
   private face: string = '';
   private logger: Logger | null = null;
   private config: LoggerConfig = {
     showService: true,
     silent: false
-  }
+  };
 
   constructor(serviceName: string, instanceName: string, path?: string, face?: string, config?: LoggerConfig) {
     if (config) {
-      this.config = {...this.config, ...config};
+      this.config = { ...this.config, ...config };
     }
     if (serviceName.length <= tagLen) {
       this.serviceName = serviceName;
@@ -46,8 +44,8 @@ class ServiceLogger {
       console.error(`Instance name cannot be longer than ${tagLen} characters`);
       process.exit(1);
     }
-    if (!face) {}
-    else if (face.length > faceLen) {
+    if (!face) {
+    } else if (face.length > faceLen) {
       this.face = face;
     } else {
       console.error(`Face string cannot be longer than ${tagLen} characters`);
@@ -55,15 +53,24 @@ class ServiceLogger {
     }
     this.path = path || this.path;
 
-    // TODO: Define type TransformableInfo 
+    // TODO: Define type TransformableInfo
     const style = printf((info: any): string => {
       const level: string = levelSign[info.level];
-      const sern: string = this.serviceName !== '' ? '@' + this.serviceName.toUpperCase() + Array(tagLen + 1 - this.serviceName.length).join(' ') : Array(tagLen + 2).join(' ');
-      const insn: string = this.instanceName !== '' ? '#' + this.instanceName.toUpperCase() + Array(tagLen + 1 - this.instanceName.length).join(' ') : Array(tagLen + 2).join(' ');
+      const sern: string =
+        this.serviceName !== ''
+          ? '@' + this.serviceName.toUpperCase() + Array(tagLen + 1 - this.serviceName.length).join(' ')
+          : Array(tagLen + 2).join(' ');
+      const insn: string =
+        this.instanceName !== ''
+          ? '#' + this.instanceName.toUpperCase() + Array(tagLen + 1 - this.instanceName.length).join(' ')
+          : Array(tagLen + 2).join(' ');
       const now: Date = new Date();
-      return `${this.config.showService ? sern : ''} ${insn}` +
-        `[${(now.getHours() < 10 ? ' ' : '') + now.getHours()}:${(now.getMinutes() < 10 ? ' ' : '') + now.getMinutes()}]` +
-        `${this.face} ${level}| ${info.message}`;
+      return (
+        `${this.config.showService ? sern : ''} ${insn}` +
+        `[${(now.getHours() < 10 ? ' ' : '') + now.getHours()}:${(now.getMinutes() < 10 ? ' ' : '') +
+          now.getMinutes()}]` +
+        `${this.face} ${level}| ${info.message}`
+      );
     });
 
     const options = {
@@ -80,79 +87,76 @@ class ServiceLogger {
         json: false,
         colorize: true
       }
-    }
+    };
 
     this.logger = createLogger({
-      format: combine(
-        timestamp(),
-        style
-      ),
-      transports: [
-        new transports.File(options.file),
-        new transports.Console(options.console)
-      ],
+      format: combine(timestamp(), style),
+      transports: [new transports.File(options.file), new transports.Console(options.console)],
       exitOnError: false
     });
   }
 
-  public info(message: any) {
+  public info = (message: any) => {
     if (this.logger != null && !this.config.silent) this.logger.info(message);
-  }
+  };
 
-  public warn(message: any) {
+  public warn = (message: any) => {
     if (this.logger != null && !this.config.silent) this.logger.warn(message);
-  }
+  };
 
-  public err(message: any) {
+  public err = (message: any) => {
     if (this.logger != null && !this.config.silent) this.logger.error(message);
-  }
+  };
 
-  public debug(message: any) {
+  public debug = (message: any) => {
     if (this.logger != null) this.logger.debug(message);
-  }
+  };
 
-  public silent(mod: boolean) {
+  public silent = (mod: boolean) => {
     this.config.silent = mod;
-  }
+  };
 }
 
 export default class LogManager {
   private serviceName: string;
-  private path: string; 
+  private path: string;
   private config: LMConfig = {
     showService: true
-  }
-  private instances: ServiceLogger[] = [];  
+  };
+  private instances: ServiceLogger[] = [];
 
   constructor(serviceName: string, path?: string, config?: LMConfig) {
     if (config) {
-      this.config = {...this.config, ...config};
+      this.config = { ...this.config, ...config };
     }
     this.serviceName = serviceName;
     this.path = path || 'logs';
   }
 
-  public getLogger(instanceName?: string, config?: LoggerConfig): ServiceLogger {
-    const instance = new ServiceLogger(this.serviceName, instanceName || '', this.path, undefined, { ...config, ...this.config});
+  public getLogger = (instanceName?: string, config?: LoggerConfig): ServiceLogger => {
+    const instance = new ServiceLogger(this.serviceName, instanceName || '', this.path, undefined, {
+      ...config,
+      ...this.config
+    });
     this.instances.push(instance);
     return instance;
-  }
+  };
 
   public mute = () => {
     this.instances.forEach((logger: ServiceLogger) => {
       logger.silent(true);
     });
-  }
+  };
 
   public unmute = () => {
     this.instances.forEach((logger: ServiceLogger) => {
       logger.silent(false);
     });
-  }
+  };
 }
 
 export interface LMConfig {
-  showService?: boolean;  
+  showService?: boolean;
 }
 
 export interface LoggerConfig {
